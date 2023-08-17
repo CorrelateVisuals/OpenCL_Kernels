3804244064cda59522f3774373ccbfd157332867
CorrelateVisuals <jakobpovel@protonmail.com>
2023-08-17 16:10:52 +0200

    Expended returned values

--- OpenCL_Device_Information/main.cpp
+++ OpenCL_Device_Information/main.cpp
@@ -1,31 +1,38 @@
 #include <CL/cl.h>
 #include <iostream>
+#include <string>
+
+void printDeviceProperty(cl_device_id device,
+                         cl_device_info property,
+                         const std::string& propertyName) {
+  size_t propertySize;
+  clGetDeviceInfo(device, property, 0, NULL, &propertySize);
+
+  char* propertyValue = new char[propertySize];
+  clGetDeviceInfo(device, property, propertySize, propertyValue, NULL);
+
+  std::cout << propertyName << ": " << propertyValue << std::endl;
+
+  delete[] propertyValue;
+}
 
 void printDeviceProperties(cl_device_id device) {
-  char name[1024];
-  clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(name), name, NULL);
+  printDeviceProperty(device, CL_DEVICE_NAME, "Device Name");
+  printDeviceProperty(device, CL_DEVICE_VENDOR, "Vendor");
+  printDeviceProperty(device, CL_DEVICE_VERSION, "Device Version");
+  printDeviceProperty(device, CL_DRIVER_VERSION, "Driver Version");
 
-  cl_ulong totalGlobalMem, sharedMemPerBlock, totalConstMem;
-  cl_uint regsPerBlock, warpSize, memPitch, maxThreadsPerBlock, major, minor,
-      clockRate, textureAlignment, deviceOverlap, multiProcessorCount;
-  size_t maxThreadsDim[3], maxGridSize[3], maxTexture1D, maxTexture2D[2],
-      maxTexture3D[3];
-  clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(totalGlobalMem),
-                  &totalGlobalMem, NULL);
-  clGetDeviceInfo(device, CL_DEVICE_LOCAL_MEM_SIZE, sizeof(sharedMemPerBlock),
-                  &sharedMemPerBlock, NULL);
-  clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE,
-                  sizeof(maxThreadsPerBlock), &maxThreadsPerBlock, NULL);
-  clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(maxThreadsDim),
-                  &maxThreadsDim, NULL);
-  clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS,
-                  sizeof(multiProcessorCount), &multiProcessorCount, NULL);
+  cl_ulong globalMemSize;
+  clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(globalMemSize),
+                  &globalMemSize, NULL);
+  std::cout << "Global Memory Size: " << globalMemSize << " bytes" << std::endl;
 
-  std::cout << "Device Name: " << name << std::endl;
-  std::cout << "Total Global Memory: " << totalGlobalMem << " bytes"
-            << std::endl;
-  std::cout << "Shared Memory Per Block: " << sharedMemPerBlock << " bytes"
-            << std::endl;
+  cl_uint maxComputeUnits;
+  clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits),
+                  &maxComputeUnits, NULL);
+  std::cout << "Max Compute Units: " << maxComputeUnits << std::endl;
+
+  std::cout << std::endl;
 }
 
 int main() {
@@ -37,8 +44,8 @@ int main() {
   clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 10, devices, &deviceCount);
 
   for (cl_uint i = 0; i < deviceCount; i++) {
+    std::cout << "Device " << i + 1 << ":" << std::endl;
     printDeviceProperties(devices[i]);
-    std::cout << std::endl;
   }
 
   return 0;
